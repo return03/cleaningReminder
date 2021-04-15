ffc86555dcd92d8a9c3c8b1325c46fe0168f2be0
return03 <d.rottmaier@gmail.com>
2021-04-15 18:25:21 +0200

    comments added

--- bigC_Analysis.py
+++ bigC_Analysis.py
@@ -1,4 +1,5 @@
-#from selenium import webdriver
+
+from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
@@ -6,33 +7,52 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By 
 import time 
 import random
-from sheetsAPI import sheets
+from sheetsAPI import sheets    #contains the class for the google sheets api
 
+#Open the file that contains the ms since the epoch of the last time the script was fully executed
+#Fully executed means the if-statement below was entered
 time_saver=open('C:/Users/drott/DS_Python/.venv/build/exe.win-amd64-3.9/last_ts.txt','r+')
 last_ts=int(time_saver.read())
-cur_t=time.gmtime()
-cur_ts=int(time.mktime(cur_t))
-last_ts=1
+cur_ts=int(time.mktime(time.gmtime()))
+
+#if the time (in ms) between the time saved in the last_ts.txt-file 
+# and the current time exceeds 1814400 ms (3 weeks time) enter the loop
 if cur_ts - last_ts > 1814400:
-    last_ts=cur_ts
+
+    #Overwrite the time in the last_ts.txt-file with the current one
+    #as the new reference
     time_saver.seek(0)
-    time_saver.writelines(str(last_ts))
+    time_saver.writelines(str(cur_ts))
     time_saver.truncate()
     time_saver.close()
 
+    #Call the constructor of the class sheets and connect to 
+    #the goolge sheets-api and the sheet and read the values 
+    #with the room no and tenants from it
     sheet=sheets()
     sheet.authenticate()
     values=sheet.getSheetValue()
     for row in values:
-        # Print columns A and E, which correspond to indices 0 and 4.
+        # here comes a loop that assigns the right name 
+        # to the lists of ground_tenants & high_tenants based on the room no. 
+        # the separation is needed because the ground_tenants and high_tenants 
+        # have tasks that are seperated from another
+        # ground_tenants contain the tenants that live on ground and first floor
+        # high_tenants contain the tenants that live on second floor
         print('%s' % (row[0]))
+    
     ground_tenants=["Narayan", "Akash", "Russ","Deepak", "Daniela"]
+    
     high_tenants=["Andreas", "Philipp"]
     tasks=["Ground Floor", "Bathroom 1st floor", "3rd floor", "staircases"]
+    # shuffle the order of ground_tenant to assure that not everyone needs 
+    # to do the same task everytime
     random.shuffle(ground_tenants)
 
+    # construct the string for the msg 
     string="Hello this is your automatic cleaning reminder, %s is assigned to %s & %s & %s, %s is assigned to %s and the %s is assigned to %s while %s goes to %s & %s, due date is the upcoming sunday evening. \n Toilet paper is bought by %s Have fun and Cheers! Python"%(tasks[0],ground_tenants[0],ground_tenants[1],ground_tenants[2],tasks[1],ground_tenants[3],tasks[3],ground_tenants[4],tasks[2],high_tenants[0], high_tenants[1], ground_tenants[0])
 
+    # open chrome, start whatsapp and sent the string in a msg
     options = webdriver.ChromeOptions()
     options.add_argument("--user-data-dir=C:/Users/drott/AppData/Local/Google/Chrome/User Data/")
     driver = webdriver.Chrome(executable_path='C:/Users/drott/DS_Python/.venv/chromedriver.exe',chrome_options=options)  # Optional argument, if not specified will search path.
